4ef02143778d595339d04022a838c299479e8967
Yuto299 <ota.se.ke@outlook.jp>
2025-05-05 17:34:41 +0900

    実装完了

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,39 @@ import TodoList from './TodoList';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [todos, setTodos] = useState([{ id: 1, name: 'Todo1', completed: false }]);
+  const [todos, setTodos] = useState([]); // タスクの配列を初期化
 
   const todoNameRef = useRef();
 
   const handleAddTodo = () => {
     //タスクを追加する
     const name = todoNameRef.current.value;
+    if (name === '') return; // 空のタスクは追加しない
     setTodos((prevTodos) => {
       return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
     });
     todoNameRef.current.value = null;
   };
 
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos]; // 配列のコピーを作成
+    const todo = newTodos.find((todo) => todo.id === id); // idが一致するtodoを探す
+    todo.completed = !todo.completed; // 完了状態を反転させる
+    setTodos(newTodos);
+  };
+
   return (
     <>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input type='text' ref={todoNameRef} />
       <button onClick={handleAddTodo}>タスクを追加</button>
-      <button>完了したタスクの削除</button>
-      <div>残りのタスク:0</div>
+      <button onClick={handleClear}>完了したタスクの削除</button>
+      <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
     </>
   );
 }
